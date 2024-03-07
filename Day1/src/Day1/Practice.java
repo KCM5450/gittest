71ecda4947eaf742f37f03e983db1ca957a945cb
kcm <kcm5450>
2024-03-07 16:38:19 +0900

    3월7일 공부

--- Day1/src/Day1/Practice.java
+++ Day1/src/Day1/Practice.java
@@ -1,18 +1,27 @@
 package Day1;
 import java.util.Scanner;
 public class Practice {
-	public static void main(String[] args) {
+			
+			Scanner sc;
+			sc = new Scanner(System.in);
+			int a,b;
+			String result;
+			
+		public Practice() {
+			System.out.print("숫자를 입력하세요:");
+			a = sc.nextInt();
+		}
+		public void Practice() {
+			System.out.print("연산자를 입력하세요:");
+			result = sc.next();
+		}
+		public void Practice() {
+			System.out.print("숫자를 입력하세요:");
+			b = sc.nextInt();
+		}
 		
-		Scanner sc;
-		sc = new Scanner(System.in);
-
-		System.out.print("숫자를 입력하세요:");
-		int a = sc.nextInt();
-		System.out.print("연산자를 입력하세요:");
-		String result = sc.next();
-		System.out.print("숫자를 입력하세요:");
-		int b = sc.nextInt();
-
+		
+		public void Practice() {
 		switch (result) {
 		case "+":
 			System.out.println(a+b);
@@ -28,5 +37,15 @@ public class Practice {
 			break;
 		default: System.out.println("잘못된 입력");
 		}
+		public static void main(String[] args) {
+			Practice pr = new Practice();
+			
+		}
 	}
 }
+
+        //int a = sc.nextInt();
+
+		//String result = sc.next();
+	
+		//int b = sc.nextInt();
